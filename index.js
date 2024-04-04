91bbb2dca7498add17e1877276bcc6568fef12a4
Peter <peterkanyi19193@gmail.com>
2024-04-04 12:33:26 +0300

    adds dom modifying elements lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+
+const remove =document.querySelector('#main')
+remove.remove();
+
+const newHeader = document.createElement("h1")
+newHeader.id ="victory"
+newHeader.innerHTML = "Kanyi is the champion"
